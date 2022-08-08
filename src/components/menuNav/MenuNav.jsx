43fbc35343f40c21c3bfe336a712c88be3ca558c
ferbargar99@hotmail.com <ferbargar99@hotmail.com>
2022-08-07 21:04:29 -0500

    Cambios responsivos en Menu y Slider

--- src/components/menuNav/MenuNav.jsx
+++ src/components/menuNav/MenuNav.jsx
@@ -9,7 +9,8 @@ export function MenuNav() {
     const [isNavExpanded, setIsNavExpanded] = useState(false)
 
   return (
-        <div className='container'>
+        <>
+        <div className='menu-container'>
             <nav className="navigation">
 
                 {/* Toogle button */}
@@ -31,16 +32,15 @@ export function MenuNav() {
                     </svg>
                 </div>
 
-                
                 {/* Routes */}
-                <div className={ isNavExpanded ? "navigation-menu expanded" : "navigation-menu"}>
+                <div className={ isNavExpanded ? "d-none" : "navigation-menu"}>
                     <ul>
                         <li>
                             <a href="/home">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-person" viewBox="0 0 16 16">
                                     <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
                                 </svg>
-                                Mi cuenta
+                                <span>Mi cuenta</span>
                             </a>
                         </li>
                     </ul>
@@ -54,5 +54,12 @@ export function MenuNav() {
                 </div>
             </nav>
         </div>
+        <div className='form-search-sm'>
+                    <input type="text" className='form-input' placeholder='Buscar producto...'/>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search lupa" viewBox="0 0 16 16">
+                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                    </svg>
+            </div>
+        </>
   )
 }
